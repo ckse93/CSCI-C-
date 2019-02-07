219f05655f5d25aa0ee0da249b613f1dcf4b061d
Chan Jung <ckse93@gmail.com>
2019-02-06 23:49:39 -0600

    Update ptr_and_memalloc_prac.cc

--- ptr_and_memalloc_prac.cc
+++ ptr_and_memalloc_prac.cc
@@ -5,7 +5,7 @@ using namespace std;
 int main (){
 	int * foo; 							//declaring pointer
 	foo = new (nothrow) int[MAXLEN];  	// now foo is pointing at the 0th element of an array
-										// (nothrow) prevents the program from shutting down when heap space doesnt have enough memory
+	int * fooclone = foo; 							// (nothrow) prevents the program from shutting down when heap space doesnt have enough memory
 										// if there is not enough space in heap, it will just return nullptr
 										//dynamically allocated stuffs are in heap space, others are in stack. 
 	if (foo == nullptr){
@@ -13,13 +13,15 @@ int main (){
 		}
 	for (int i = 0 ; i<MAXLEN ; i++){	//conventional way to access array element
 		foo[i] = i+10;
+		cout << *(fooclone)<<endl;
+		fooclone++;
 		}
 	cout << "printing the array element..."<<endl<<endl;
 	
 	for (int j = 0 ; j<MAXLEN ; j++){
 		cout << *foo+(j) << endl;		//differnt way to access array element. it has to be a pointer type 
 		}
-	delete[] foo;						//delete function deallocates the allocated element. 
+	delete[] foo;						                //delete function deallocates the allocated element. 
 										//delete[] deallocates the array of allocated stuffs 
 									
 	return 0;
