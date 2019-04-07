db3e05228e1881720d2e19a0ee95527c633d0a44
Chan Jung <ckse93@gmail.com>
2019-04-06 19:45:55 -0500

    Create math_vector.cc

---
+++ template/math_vector.cc
@@ -0,0 +1,20 @@
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+#include "math_vector.h"
+
+MathVector::MathVector(double x, double y) : x_(x), y_(y) {
+  length_ = pow(x_*x_ + y_*y_, 0.5);
+}
+
+// >>>> WRITE THIS overloaded operator using length
+bool MathVector::operator<(const MathVector& v) {
+  return true;
+}
+
+std::ostream& operator<<(std::ostream& os, const MathVector& v) {
+  return os << "|" << v.x_ << "," << v.y_ << "| = " << v.length_;
+}
