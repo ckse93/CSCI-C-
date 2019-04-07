fd190dfaf9c424f53df652b2d9498f8c950c35ac
Chan Jung <ckse93@gmail.com>
2019-04-06 19:43:40 -0500

    Create circle.cc

---
+++ template/circle.cc
@@ -0,0 +1,17 @@
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+
+#include "circle.h"
+
+Circle::Circle(double r) : radius_(r), color_(kBlue) {}
+
+// >>>> WRITE THIS overloaded operator using radius
+bool Circle::operator<(const Circle& rhs) {
+  return true;
+}
+
+std::ostream& operator<<(std::ostream& os, const Circle& c) {
+  return os << "radius = " << c.radius_;
+}
